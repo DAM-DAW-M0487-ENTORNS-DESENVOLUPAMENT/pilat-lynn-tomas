1d4fdf5033a3b000a2f258a2a8bd88b19d3dd4ea
Lynn T. <lynn.tatari@gmail.com>
2025-04-28 12:28:16 +0200

    Commit 1 -test

---
+++ Entorns - Git TaulaLlista/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Entorns___Git_TaulaLlista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
